261663cc47d0525c83c423f0e6d52cb683928117
EMai21 <40373304+EMai21@users.noreply.github.com>
2018-07-10 13:56:27 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,15 @@
+
+def add(x,y):
+    return x+y
+def diff(x,y):
+    return x-y
+def product(x,y):
+    return x*y
+def greatest(x,y):
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    else:
+        return "They are equal"
+
